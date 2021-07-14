374ec2db21e1fc9c07194e95c6e2b3093a5513d2
«Alexander <unw1ck3d@yandex.ru>
2021-07-14 17:27:20 +0300

    second

--- Lesson_1/credit.py
+++ Lesson_1/credit.py
@@ -2,11 +2,9 @@
 
 name1 = input("Введите имя первого человека: ")
 name2 = input("Введите имя второго человека: ")
-name3 = input("Введите имя третьего человека: ")
 
 salary1 = input("зп первого: ")
 salary2 = input("зп второго: ")
-salary3 = input("зп третьего: ")
 
 credit = float(input("Введите сумму кредита: "))
 period = float(input("Введите срок в месяцах: "))
@@ -16,4 +14,3 @@ pay_per_month = credit / period + (credit / 100 * procent) / 12
 print("Ежемесячный платёж составит -", pay_per_month, "рублей")
 print(name1, "сможет потратить", float(salary1) - pay_per_month)
 print(name2, "сможет потратить", float(salary2) - pay_per_month)
-print(name3, "сможет потратить", float(salary3) - pay_per_month)
